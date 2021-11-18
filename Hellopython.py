333fc55a79fb7bdedc1c1364a3cb4ae9176938db
Joey <93352392+BrekTech@users.noreply.github.com>
2021-11-18 23:16:19 +0000

    Fun little Conversation

--- Hellopython.py
+++ Hellopython.py
@@ -1,1 +1,20 @@
-print("Hello World!")
+print("Hello You!")
+print("What is your name?")
+
+name= input()
+print(f"Hi there {name}")
+print("How old are you "+name+"?" )
+
+age=input()
+print("Why are you "+age+"?")
+
+NotImportant= input()
+print("You are strange "+name)
+print("What's your stance on Hats?")
+
+Stance_on_hats= input() 
+print("I agree")
+print("What\'s your name again?")
+
+false_name=input()
+print(f"That's not your name. Earlier you said your name was \"{name}\" ")
