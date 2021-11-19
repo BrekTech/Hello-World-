8d2558c315605e2460ae704733a93db03205cf86
Joey <93352392+BrekTech@users.noreply.github.com>
2021-11-19 06:01:36 +0000

    Added A LOT more convsersation and began making branches of conversations

--- Hellopython.py
+++ Hellopython.py
@@ -1,34 +1,124 @@
-print("Hello You!")
+print("K- Hello You!\nMy name is Kevin.")
 print("What is your name?")
 print()
 
 name= input()
 print()
-
-print(f"Hi there {name}")
+if name == "Kevin": print("K- Wow, we got the same name. Nice to meet you Kevin")
+else:
+  print(f"K- Hi there {name}")
 print("How old are you?" )
 print()
 
 age=input()
 print()
 
-print("Why are you "+age+"?")
+print("K- Why are you "+age+"?")
 print()
 
 NotImportant= input()
 print()
 
-print("You are strange "+name)
-print("What's your stance on Hats?")
+print("K- You are strange "+name)
+print("Do you like hats?")
 print()
 
 Stance_on_hats= input() 
 print()
 
-print("I agree")
-print("What\'s your name again?")
+if Stance_on_hats == "Yes"or"yes" :
+  Stance_on_hats = "like"
+  pass
+else:
+  if Stance_on_hats == "No"or"no":
+    Stance_on_hats = "dislike"
+    pass
+  else: 
+    Stance_on_hats = "NA"
+
+print("K- I agree")
+
+if name == "Kevin":
+  print ("One last question. Are you having a good day?")
+  print() 
+  pass
+else:
+  print("What\'s your name again?")
+  print()
+  
+  previous_input=input()
+  print()
+
+  if name == previous_input: 
+    print ("K- One last question. Are you having a good day?")
+    print()
+    pass
+  else: 
+    print(f"K- Liar!!! You said your name was {name}! \nYou can't trick me \nSo which one is it? {name} or {previous_input}?")
+    print()
+
+    name_choice=input()
+    print()
+    if name_choice == name:
+      name = name 
+      print ("K- One last question. Are you having a good day?")
+      print()
+      pass
+
+    else:
+      if name_choice == previous_input:
+        name = previous_input
+        print ("K- One last question. Are you having a good day?")
+        print()
+        pass 
+      
+      else:
+        print(f"K- No, you only got 2 options. {name} or {previous_input}. Which one is it?")
+        print()
+
+        name_choice=input()
+        print()
+
+        if name_choice == name: 
+          print (f"K- Ok then, your name is {name}")
+          pass
+        
+        else: 
+          if name_choice == previous_input:
+            name = previous_input
+            print (f"K- Ok then, your name is {name}")
+            pass
+          
+          else:
+            print("K- Whatever, I will just call you Smith")
+            name = "Smith"
+        print ("One last question. Are you having a good day?")
+        print()          
+
+
+Opinion_on_day=input()
 print()
 
-false_name=input()
-print(f"That's not your name. Earlier you said your name was \"{name}\" ")
-print
+if Opinion_on_day == "Yes"or"yes" :
+  print("K- That's Wonderful!")
+  Opinion_of_day = "good day"
+  pass
+else:
+  if Opinion_on_day == "No"or"no":
+    print("K- I'm sorry to hear that" )
+    Opinion_of_day = "bad day"
+    pass
+  else: 
+    print("K- Interesting.")
+    Opinion_of_day = "NA"
+
+print()
+print(f"SUMMARY...\nYour name is {name}.\nYou are {age} years old.")
+if Opinion_of_day == "NA":
+  pass
+else:
+  print(f"You are having a {Opinion_of_day}.")
+if Stance_on_hats == "NA":
+  pass
+else:
+  print(f"And you {Stance_on_hats} hats.")
---
+++ Tests.py
@@ -0,0 +1,13 @@
+print("Welcome. What is your name?")
+print()
+
+name1=input()
+print()
+
+print("I'm sorry, can you say that one more time?")
+print()
+
+name=input()
+print()
+
+print(f"Ah, so it was {name}")if name == name1 else print(f"Huh? I could of swore I heard {name1}. Oh well, I gues your name is {name}")
