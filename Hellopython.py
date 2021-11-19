1ae98593bdc184a14b2835676fdd00a995a25d30
Joey <93352392+BrekTech@users.noreply.github.com>
2021-11-19 01:29:00 +0000

    Fixed the spacing between people talking

--- Hellopython.py
+++ Hellopython.py
@@ -1,22 +1,34 @@
 print("Hello You!")
 print("What is your name?")
 print()
+
 name= input()
 print()
+
 print(f"Hi there {name}")
-print("How old are you "+name+"?" )
+print("How old are you?" )
 print()
+
 age=input()
 print()
+
 print("Why are you "+age+"?")
+print()
 
 NotImportant= input()
+print()
+
 print("You are strange "+name)
 print("What's your stance on Hats?")
+print()
 
 Stance_on_hats= input() 
+print()
+
 print("I agree")
 print("What\'s your name again?")
+print()
 
 false_name=input()
 print(f"That's not your name. Earlier you said your name was \"{name}\" ")
+print
